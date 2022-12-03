af4dad2d4cd8ae86f3e6b0216a2161a461a820dd
Gustavo Gonzaga <94642849+GonzagaGustavo@users.noreply.github.com>
2022-12-03 14:24:43 -0300

    .

--- main.js
+++ main.js
@@ -1,46 +1,49 @@
-import * as THREE from 'three'
-import { camera } from './src/camera'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import Stats from 'three/addons/libs/stats.module.js';
-import './src/move'
+import * as THREE from "three";
+import { camera } from "./src/camera";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import Stats from "three/addons/libs/stats.module.js";
+import "./src/move";
+import move, { handleMouseMove } from "./src/move";
 
-const scene = new THREE.Scene()
-let stats = new Stats()
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.shadowMap.enabled = true
-document.body.appendChild( renderer.domElement )
-renderer.setAnimationLoop( animate );
+const scene = new THREE.Scene();
+let stats = new Stats();
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+renderer.setAnimationLoop(animate);
 
-let controls = new OrbitControls( camera, renderer.domElement );
+let controls = new OrbitControls(camera, renderer.domElement);
 controls.autoRotate = true;
 // cube camera
-let cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 256 );
-let cubeCamera = new THREE.CubeCamera( 1, 1000, cubeRenderTarget );
-// cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: "brown"})
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-const geometry2 = new THREE.DodecahedronGeometry(1, 1, 1)
-const material2 = new THREE.MeshBasicMaterial({ color: "yellow"})
-const circle = new THREE.Mesh(geometry2, material2)
-circle.position.y = 5
-scene.add(circle)
+let cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256);
+let cubeCamera = new THREE.CubeCamera(1, 1000, cubeRenderTarget);
+// floor
 
-const geometry3 = new THREE.ConeGeometry(1, 4.5, 5)
-const material3 = new THREE.MeshBasicMaterial({ color: "rgb(0, 128, 0)"})
-const cone = new THREE.Mesh(geometry3, material3)
-cone.position.y = 2.5
-scene.add(cone)
+const floorTexture = new THREE.TextureLoader().load("/textures/coblestone.jpg");
+floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+floorTexture.repeat.set(20, 20);
+const floorMaterial = new THREE.MeshBasicMaterial({
+  map: floorTexture,
+  side: THREE.DoubleSide,
+});
+const floorGeometry = new THREE.PlaneGeometry(20, 20, 20, 20);
+let floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.position.x = 2;
+floor.position.y = -0.5;
+floor.position.z = 3;
+floor.rotation.x = (-90 * Math.PI) / 180;
+scene.add(floor);
+// mouse move
+document.onmousemove = handleMouseMove;
 
-camera.position.z = 10
-camera.position.y = 2
-
+camera.position.z = 10;
+camera.position.y = 2;
 function animate() {
-  requestAnimationFrame(animate)
-  cubeCamera.update( renderer, scene );
-  renderer.render(scene, camera)
-  stats.update()
+  requestAnimationFrame(animate);
+  move();
+  cubeCamera.update(renderer, scene);
+  renderer.render(scene, camera);
+  stats.update();
 }
-animate()
+animate();
--- src/move.js
+++ src/move.js
@@ -1,31 +1,37 @@
-import * as THREE from "three";
 import { camera } from "./camera";
 import { delay } from "./utils";
-const x = camera.position.x;
-const y = camera.position.x;
-const z = camera.position.z;
+let keydown = {};
 
-function jump() {
-  camera.position.y += 1;
-  delay(100)
-  camera.position.y -= 1;
+export default function move() {
+  window.onkeydown = (e) => {
+    keydown[e.key.toLowerCase()] = true;
+  };
+  window.onkeyup = (e) => {
+    keydown[e.key.toLowerCase()] = false;
+  };
+  if (keydown["w"]) camera.position.z -= 0.1;
+  if (keydown["s"]) camera.position.z += 0.1;
+  if (keydown["d"]) camera.position.x += 0.1;
+  if (keydown["a"]) camera.position.x -= 0.1;
 }
+export const handleMouseMove = (e) => {
+  let eventDoc, doc, body;
 
-document.addEventListener(
-  "keydown",
-  (e) => {
-    const key = e.key.toLowerCase();
-    if (key == "a") {
-      camera.position.x -= 0.1;
-    } else if (key == "d") {
-      camera.position.x += 0.1;
-    } else if (key == "w") {
-      camera.position.z -= 0.1;
-    } else if (key == "s") {
-      camera.position.z += 0.1;
-    } else if (key == " ") {
-      jump()
-    }
-  },
-  false
-);
+  if (e.pageX == null && e.clientX != null) {
+    eventDoc = (e.target && e.target.ownerDocument) || document;
+    doc = eventDoc.documentElement;
+    body = eventDoc.body;
+
+    e.pageX =
+      e.clientX +
+      ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
+      ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0);
+
+    e.pageYevent.pageY =
+      e.clientY +
+      ((doc && doc.scrollTop) || (body && body.scrollTop) || 0) -
+      ((doc && doc.clientTop) || (body && body.clientTop) || 0);
+  }
+  eventPageX -= e.movementX * 0.2;
+  eventPageY -= e.movementY * 0.2;
+};
